056f213ca201d80c90a3c0f4e3001a0326bd1458
Micah Yong <micahtyong@berkeley.edu>
2019-05-23 16:47:27 -0700

    JSON integration static

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 750) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className = {this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className = "HeaderGroup">
+          <img src = {require('../images/Icon.png')} 
+          width = "30"></img>
+          <Link to = "/work">Work</Link>
+          <Link to = "/about">About</Link>
+          {/* <Link to = "/about"><button>About</button></Link> */}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
